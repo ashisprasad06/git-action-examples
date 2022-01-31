aaa058c4313619f35348b4757b2f536bf58a1986
Ashis Prasad <ashis.prasad@sainsburys.co.uk>
2022-01-31 18:37:05 +0000

    add table 6

--- update/sql/tables.sql
+++ update/sql/tables.sql
@@ -22,3 +22,8 @@ create table ${database_name}.${legacy_schema}.TBL_5 (
     col_i integer,
     col_j integer
 );
+
+create table ${database_name}.${legacy_schema}.TBL_6 (
+    col_i integer,
+    col_j integer
+);
