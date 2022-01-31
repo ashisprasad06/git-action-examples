db9a3f5c6f74c962f03797e8fb80eeab6e2529be
Ashis Prasad <47937504+ashisprasad06@users.noreply.github.com>
2022-01-31 18:58:46 +0000

    add table 7 (#48)
    * add table 7
    
    * change column
    
    * change column
    
    * change column

--- update/sql/tables.sql
+++ update/sql/tables.sql
@@ -28,6 +28,11 @@ create table ${database_name}.${legacy_schema}.TBL_6 (
     col_m integer
 );
 
+create table ${database_name}.${legacy_schema}.TBL_7 (
+    col_x integer,
+    col_y integer
+ );
+
 create table ${database_name}.${legacy_schema}.TBL_8 (
     col_i integer,
     col_j integer
