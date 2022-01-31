d98da2269492ab77352bd175b6feaa62716354c4
Ashis Prasad <ashis.prasad@sainsburys.co.uk>
2022-01-31 16:23:01 +0000

    Add new github action

---
+++ update/sql/tables.sql
@@ -0,0 +1,24 @@
+create table ${database_name}.${legacy_schema}.TBL_1 (
+    col_a integer,
+    col_b integer
+);
+
+create table ${database_name}.${legacy_schema}.TBL_2 (
+    col_c integer,
+    col_d integer
+);
+
+create table ${database_name}.${legacy_schema}.TBl_3 (
+    col_e integer,
+    col_f integer
+);
+
+create table ${database_name}.${legacy_schema}.TBL_4 (
+    col_g integer,
+    col_h integer
+);
+
+create table ${database_name}.${legacy_schema}.TBL_5 (
+    col_i integer,
+    col_j integer
+);
